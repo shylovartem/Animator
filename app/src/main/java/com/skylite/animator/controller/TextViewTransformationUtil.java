170e7d5f6adbfba4523a3b766984aeadbefd28e3
ascrking@gmail.com <sa@>
2023-10-18 17:45:00 +0300

    resolved

--- app/src/main/java/com/skylite/animator/controller/TextViewTransformationUtil.java
+++ app/src/main/java/com/skylite/animator/controller/TextViewTransformationUtil.java
@@ -8,11 +8,14 @@ import android.widget.TextView;
 
 /**
  * Class contains methods that change appearance of TextView
+ * @
  */
 public class TextViewTransformationUtil {
 
     /**
-     * Change text color by click
+     * Change color of text according task
+     *
+     * @param text_label view, that need to modify
      */
     public static void changeTextColor(TextView text_label) {
         if (text_label.getResources().getConfiguration().getLocales().get(0).getLanguage().equals("ru")) {
@@ -23,6 +26,8 @@ public class TextViewTransformationUtil {
     }
 
     /**
+     * Method returns height of navigation bar
+     *
      * @param context for request context resources
      * @return height of navigation bar
      */
